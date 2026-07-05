56026d92d0cf7cce8407230baf7bf6faa7e4cd9a
theYugrathee <yugrathi100@gmail.com>
2026-07-05 10:59:44 +0530

    Fix robots.txt and implement static sitemap for Google Search Console

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,40 +1,12 @@
-import type { MetadataRoute } from "next";
-import { getSiteUrl } from "@/lib/constants";
-
-const DISALLOW_PATHS = ["/api/", "/draft/", "/_draft/"];
-
-const SEARCH_ENGINE_BOTS = ["Googlebot", "Bingbot", "Baiduspider"];
-
-const AI_ANSWER_ENGINE_BOTS = [
-  "GPTBot",
-  "PerplexityBot",
-  "ClaudeBot",
-  "Google-Extended",
-  "Applebot-Extended",
-];
-
-function buildAllowRule(userAgent: string) {
-  return {
-    userAgent,
-    allow: "/",
-    disallow: DISALLOW_PATHS,
-  };
-}
+import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl = getSiteUrl();
-
   return {
-    rules: [
-      ...SEARCH_ENGINE_BOTS.map(buildAllowRule),
-      ...AI_ANSWER_ENGINE_BOTS.map(buildAllowRule),
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: DISALLOW_PATHS,
-      },
-    ],
-    sitemap: `${siteUrl}/sitemap.xml`,
-    host: siteUrl,
-  };
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/draft/', '/_draft/'],
+    },
+    sitemap: 'https://uniconfesss.vercel.app/sitemap.xml',
+  }
 }
